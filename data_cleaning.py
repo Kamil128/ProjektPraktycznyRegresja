7bd44231be1bd166f0e1eb1a4f04652b6dce55b5
Kamil128 <21.kamil.pabijan@gmail.com>
2022-06-21 14:24:20 +0200

    + project

---
+++ data_cleaning.py
@@ -0,0 +1,8 @@
+def Cleaning:
+    def __init__(self, **kwargs):
+        pass
+    
+    def clean(self, **kwargs):
+        print('cleaning data...')
+        return None
+
---
+++ get_data.py
@@ -0,0 +1,7 @@
+def Data:
+    def __init__(self, **kwargs):
+        pass
+    
+    def get_data(self, **kwargs):
+        print('downloading data...')
+        return None
