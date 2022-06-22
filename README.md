21677fb52b022f4f44da5e4ba36c8fdb622e0929
Kamil128 <21.kamil.pabijan@gmail.com>
2022-06-22 17:14:20 +0200

    + tytanic

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
 # ProjektPraktycznyRegresja
+
+### Tytanic Dataset
+
--- get_data.py
+++ get_data.py
@@ -1,7 +1,30 @@
-def Data:
-    def __init__(self, **kwargs):
-        pass
+import pandas as pd
+import os
+from time import time
+
+
+class Data:
+    def __init__(self,
+                 path: str = None,
+                 save_path: str = None,
+                 file_name: str = None):
+        self.path = path
+        self.save_path = save_path
+        self.file_name = file_name
     
-    def get_data(self, **kwargs):
+    def get_data(self, path: str = None):
+        if self.path:
+            path = self.path
+        if not path:
+            raise "No path!"
+
         print('downloading data...')
-        return None
+        data = pd.read_csv(path)
+
+        if self.save_path:
+            print('saving data...')
+            if self.file_name:
+                data.to_pickle(os.path.join(self.save_path, self.file_name))
+            else:
+                data.to_pickle(os.path.join(self.save_path, f'data_{round(time())}.pkl'))
+        return data
